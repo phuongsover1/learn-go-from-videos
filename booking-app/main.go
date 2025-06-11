d08d4a46ab5dc4c65b4d6a321399abbafa7761f0
Phuong Nguyen <phuongsover2@gmail.com>
2025-06-11 20:56:51 +0700

    init package

---
+++ booking-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Println("We have total of", conferenceTickets,"tickets and", remainingTickets, "are still available")
+	fmt.Println("Get you tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+}
